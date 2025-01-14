397be4691adc0c957b2b65a556a03605ee224226
Johannes Barthold <valentin.j.schmidt@web.de>
2025-01-14 12:05:51 +0100

    implents dqn inside the dqn

--- src/agentic_dqn.py
+++ src/agentic_dqn.py
@@ -1,4 +1,3 @@
-
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
@@ -6,30 +5,56 @@ import itertools
 import time
 import torch
 import pylab as plt
+
 # %matplotlib inline
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-import memory as mem   
-from feedforward import Feedforward
 
-def running_mean(x, N):
-    cumsum = np.cumsum(np.insert(x, 0, 0)) 
-    return (cumsum[N:] - cumsum[:-N]) / float(N) 
+# class to store transitions
+class Memory:
+    def __init__(self, max_size=100000):
+        self.transitions = np.asarray([])
+        self.size = 0
+        self.current_idx = 0
+        self.max_size = max_size
+
+    def add_transition(self, transitions_new):
+        if self.size == 0:
+            blank_buffer = [np.asarray(transitions_new, dtype=object)] * self.max_size
+            self.transitions = np.asarray(blank_buffer)
+
+        self.transitions[self.current_idx, :] = np.asarray(
+            transitions_new, dtype=object
+        )
+        self.size = min(self.size + 1, self.max_size)
+        self.current_idx = (self.current_idx + 1) % self.max_size
+
+    def sample(self, batch=1):
+        if batch > self.size:
+            batch = self.size
+        self.inds = np.random.choice(range(self.size), size=batch, replace=False)
+        return self.transitions[self.inds, :]
+
+    def get_all_transitions(self):
+        return self.transitions[0 : self.size]
+
 
 class Feedforward(torch.nn.Module):
     def __init__(self, input_size, hidden_sizes, output_size):
         super(Feedforward, self).__init__()
         self.input_size = input_size
-        self.hidden_sizes  = hidden_sizes
-        self.output_size  = output_size
+        self.hidden_sizes = hidden_sizes
+        self.output_size = output_size
         layer_sizes = [self.input_size] + self.hidden_sizes
-        self.layers = torch.nn.ModuleList([ torch.nn.Linear(i, o) for i,o in zip(layer_sizes[:-1], layer_sizes[1:])])
-        self.activations = [ torch.nn.Tanh() for l in  self.layers ]
+        self.layers = torch.nn.ModuleList(
+            [torch.nn.Linear(i, o) for i, o in zip(layer_sizes[:-1], layer_sizes[1:])]
+        )
+        self.activations = [torch.nn.Tanh() for l in self.layers]
         self.readout = torch.nn.Linear(self.hidden_sizes[-1], self.output_size)
 
     def forward(self, x):
-        for layer,activation_fun in zip(self.layers, self.activations):
+        for layer, activation_fun in zip(self.layers, self.activations):
             x = activation_fun(layer(x))
         return self.readout(x)
 
@@ -37,83 +62,96 @@ class Feedforward(torch.nn.Module):
         with torch.no_grad():
             return self.forward(torch.from_numpy(x.astype(np.float32))).numpy()
 
+
 class QFunction(Feedforward):
-    def __init__(self, observation_dim, action_dim, hidden_sizes=[100,100], 
-                 learning_rate = 0.0002):
-        super().__init__(input_size=observation_dim, hidden_sizes=hidden_sizes, 
-                         output_size=action_dim)
-        self.optimizer=torch.optim.Adam(self.parameters(), 
-                                        lr=learning_rate, 
-                                        eps=0.000001)
-        self.loss = torch.nn.SmoothL1Loss() # MSELoss()
-    
+    def __init__(
+        self, observation_dim, action_dim, hidden_sizes=[100, 100], learning_rate=0.0002
+    ):
+        super().__init__(
+            input_size=observation_dim,
+            hidden_sizes=hidden_sizes,
+            output_size=action_dim,
+        )
+        self.optimizer = torch.optim.Adam(
+            self.parameters(), lr=learning_rate, eps=0.000001
+        )
+        self.loss = torch.nn.SmoothL1Loss()  # MSELoss()
+
     def fit(self, observations, actions, targets):
-        self.train() # put model in training mode
+        self.train()  # put model in training mode
         self.optimizer.zero_grad()
         # Forward pass
         acts = torch.from_numpy(actions)
         pred = self.Q_value(torch.from_numpy(observations).float(), acts)
         # Compute Loss
         loss = self.loss(pred, torch.from_numpy(targets).float())
-        
+
         # Backward pass
         loss.backward()
         self.optimizer.step()
         return loss.item()
-    
+
     def Q_value(self, observations, actions):
-        return self.forward(observations).gather(1, actions[:,None])        
-    
+        return self.forward(observations).gather(1, actions[:, None])
+
     def maxQ(self, observations):
         return np.max(self.predict(observations), axis=-1, keepdims=True)
-        
+
     def greedyAction(self, observations):
         return np.argmax(self.predict(observations), axis=-1)
 
 
-
 class DQNAgent(object):
     """
-    Agent implementing Q-learning with NN function approximation.    
+    Agent implementing Q-learning with NN function approximation.
     """
-    def __init__(self, observation_space, action_space, **userconfig):
-        
+
+    def __init__(self, observation_space, action_space, mem, **userconfig):
+
         if not isinstance(observation_space, spaces.box.Box):
-            raise UnsupportedSpace('Observation space {} incompatible ' \
-                                   'with {}. (Require: Box)'.format(observation_space, self))
+            raise UnsupportedSpace(
+                "Observation space {} incompatible "
+                "with {}. (Require: Box)".format(observation_space, self)
+            )
         if not isinstance(action_space, spaces.discrete.Discrete):
-            raise UnsupportedSpace('Action space {} incompatible with {}.' \
-                                   ' (Reqire Discrete.)'.format(action_space, self))
-        
+            raise UnsupportedSpace(
+                "Action space {} incompatible with {}."
+                " (Reqire Discrete.)".format(action_space, self)
+            )
+        self.mem = mem
         self._observation_space = observation_space
         self._action_space = action_space
         self._action_n = action_space.n
         self._config = {
-            "eps": 0.05,            # Epsilon in epsilon greedy policies                        
+            "eps": 0.05,  # Epsilon in epsilon greedy policies
             "discount": 0.95,
             "buffer_size": int(1e5),
             "batch_size": 128,
             "learning_rate": 0.0002,
             "update_target_every": 20,
-            "use_target_net":True
+            "use_target_net": True,
         }
-        self._config.update(userconfig)        
-        self._eps = self._config['eps']
-        
-        self.buffer = mem.Memory(max_size=self._config["buffer_size"])
-                
+        self._config.update(userconfig)
+        self._eps = self._config["eps"]
+
+        self.buffer = self.mem(max_size=self._config["buffer_size"])
+
         # Q Network
-        self.Q = QFunction(observation_dim=self._observation_space.shape[0], 
-                           action_dim=self._action_n,
-                           learning_rate = self._config["learning_rate"])
+        self.Q = QFunction(
+            observation_dim=self._observation_space.shape[0],
+            action_dim=self._action_n,
+            learning_rate=self._config["learning_rate"],
+        )
         # Q Network
-        self.Q_target = QFunction(observation_dim=self._observation_space.shape[0], 
-                                  action_dim=self._action_n,
-                                  learning_rate = 0)
+        self.Q_target = QFunction(
+            observation_dim=self._observation_space.shape[0],
+            action_dim=self._action_n,
+            learning_rate=0,
+        )
         self._update_target_net()
         self.train_iter = 0
-            
-    def _update_target_net(self):        
+
+    def _update_target_net(self):
         self.Q_target.load_state_dict(self.Q.state_dict())
 
     def act(self, observation, eps=None):
@@ -122,39 +160,67 @@ class DQNAgent(object):
         # epsilon greedy
         if np.random.random() > eps:
             action = self.Q.greedyAction(observation)
-        else: 
-            action = self._action_space.sample()        
+        else:
+            action = self._action_space.sample()
         return action
-    
+
     def store_transition(self, transition):
         self.buffer.add_transition(transition)
-            
+
     def train(self, iter_fit=32):
         losses = []
-        self.train_iter+=1
-        if self._config["use_target_net"] and self.train_iter % self._config["update_target_every"] == 0:
-            self._update_target_net()                
+        self.train_iter += 1
+        if (
+            self._config["use_target_net"]
+            and self.train_iter % self._config["update_target_every"] == 0
+        ):
+            self._update_target_net()
         for i in range(iter_fit):
 
             # sample from the replay buffer
-            data=self.buffer.sample(batch=self._config['batch_size'])
-            s = np.stack(data[:,0]) # s_t
-            a = np.stack(data[:,1]) # a_t
-            rew = np.stack(data[:,2])[:,None] # rew  (batchsize,1)
-            s_prime = np.stack(data[:,3]) # s_t+1
-            done = np.stack(data[:,4])[:,None] # done signal  (batchsize,1)
-            
+            data = self.buffer.sample(batch=self._config["batch_size"])
+            s = np.stack(data[:, 0])  # s_t
+            a = np.stack(data[:, 1])  # a_t
+            rew = np.stack(data[:, 2])[:, None]  # rew  (batchsize,1)
+            s_prime = np.stack(data[:, 3])  # s_t+1
+            done = np.stack(data[:, 4])[:, None]  # done signal  (batchsize,1)
+
             if self._config["use_target_net"]:
                 v_prime = self.Q_target.maxQ(s_prime)
             else:
                 v_prime = self.Q.maxQ(s_prime)
             # target
-            gamma=self._config['discount']                                                
-            td_target = rew + gamma * (1.0-done) * v_prime
-            
+            gamma = self._config["discount"]
+            td_target = rew + gamma * (1.0 - done) * v_prime
+
             # optimize the lsq objective
             fit_loss = self.Q.fit(s, a, td_target)
-            
+
             losses.append(fit_loss)
-                
+
         return losses
+
+
+class DiscreteActionWrapper(gym.ActionWrapper):
+    def __init__(self, env: gym.Env, bins=5):
+        """A wrapper for converting a 1D continuous actions into discrete ones.
+        Args:
+            env: The environment to apply the wrapper
+            bins: number of discrete actions
+        """
+        assert isinstance(env.action_space, spaces.Box)
+        super().__init__(env)
+        self.bins = bins
+        self.orig_action_space = env.action_space
+        self.action_space = spaces.Discrete(self.bins)
+
+    def action(self, action):
+        """discrete actions from low to high in 'bins'
+        Args:
+            action: The discrete action
+        Returns:
+            continuous action
+        """
+        return self.orig_action_space.low + action / (self.bins - 1.0) * (
+            self.orig_action_space.high - self.orig_action_space.low
+        )
