62215242644016644f1c5316acf8a9884d5bc684
Pablo Santos <149048926+pablo-sbl@users.noreply.github.com>
2025-01-18 19:23:43 +0100

    add save_test_results

--- my_DQN_Agent/utils/stats_functions.py
+++ my_DQN_Agent/utils/stats_functions.py
@@ -15,22 +15,28 @@ def _save_plot(env_name, name):
     plt.savefig(plot_path, dpi = 300)
     print(f"Plot saved at {plot_path}")
 
-def plot_returns(returns, env_name, name = "Returns_plot", save = True):
+def plot_returns(returns, env_name, name = "returns_plot", save = True):
 
     stats_np = np.asarray(returns)
     fig = plt.figure(figsize=(6,3.8))
     plt.plot(stats_np[:,1], label="return")
     plt.plot(running_mean(stats_np[:,1],20), label="smoothed-return")
     plt.legend()
+    plt.xlabel("Test Episode")
+    plt.ylabel("Total Reward")
+    plt.title("Reward evolution during training")
 
     if save:
         _save_plot(env_name, name)
 
-def plot_losses(losses, env_name, name = "Losses_plot", save = True):
+def plot_losses(losses, env_name, name = "losses_plot", save = True):
 
     losses_np = np.asarray(losses)
     fig = plt.figure(figsize=(6,3.8))
     plt.plot(losses_np)
+    plt.xlabel("Training step")
+    plt.ylabel("Loss")
+    plt.title("Loss evolution during training")
 
     if save:
         _save_plot(env_name, name)
@@ -57,3 +63,21 @@ def load_stats(env_name):
         loaded_losses = pickle.load(f)
 
     return loaded_stats, loaded_losses
+
+def save_test_results(env_name, test_rewards, name = "test_results", save_figure = True):
+
+    os.makedirs(f"{env_name}/stats/pkl", exist_ok=True)
+    test_path = os.path.join(f"{env_name}/stats/pkl", name)
+
+    with open(test_path, "wb") as f:
+        pickle.dump(test_rewards, f)
+
+    fig = plt.figure(figsize=(6,3.8))
+    plt.bar(range(1, len(test_rewards) + 1), test_rewards)
+    plt.xlabel("Test Episode")
+    plt.ylabel("Total Reward")
+    plt.title("Test Performance")
+    #plt.show()
+
+    if save_figure:
+        _save_plot(env_name, name)
