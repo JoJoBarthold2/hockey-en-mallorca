fad0c960da1549a7d47b5664dc9857fe9c41e32e
Pablo Santos <149048926+pablo-sbl@users.noreply.github.com>
2025-02-23 14:31:01 +0100

    Update TeM_training_only_basic.py

--- src/hockey-env/TeM_training_only_basic.py
+++ src/hockey-env/TeM_training_only_basic.py
@@ -1,4 +1,6 @@
+import os
 import time
+import copy
 import random
 import logging
 import argparse
@@ -37,6 +39,9 @@ parser.add_argument("--weights_episode", type = str, default ="", help = "Episod
 parser.add_argument("--play_against_random", action="store_true", help = "Add Random opponent to the opponents")
 parser.add_argument("--play_against_weak", action="store_true", help = "Add Weak opponent to the opponents")
 parser.add_argument("--play_against_nonweak", action="store_true", help = "Add NonWeak opponent to the opponents")
+parser.add_argument("--play_against_vales", action="store_true", help = "Add  Valentin Agent to the opponents")
+parser.add_argument("--selfplay", action="store_true", help = "Enable selfplay")
+parser.add_argument("--weights_for_selfplay", type = str, default = "", help = "Path to the weights folder that contains the starting weights for selfplay")
 #parser.add_argument("--self_play", action="store_true", help = "Add selfplay to opponents")
 args = parser.parse_args()
 
@@ -155,7 +160,7 @@ if args.agent == "Prio_DQN":
     )
 
 if args.weights != "":
-    agent.Q.load(args.weights, name=args.weights_episode)   
+    agent.Q.load(args.weights, name = args.weights_episode)   
     env_name = f"{env_name}_pretrained_from_{args.weights}_{args.weights_episode}"
 
 opponents = []
@@ -169,7 +174,24 @@ if args.play_against_weak:
     opponents_names.append("Weak")
 if args.play_against_nonweak:
     opponents.append(h_env.BasicOpponent(weak = False))
-    opponents_names.append("Nonweak")
+    opponents_names.append("NonWeak")
+if args.play_against_vales:
+    vales = Prio_DQN_Agent(
+        state_space,
+        action_space,
+        seed = seed,
+        eps = 0.01,
+        learning_rate = 0.0001,
+        hidden_sizes = [256, 256],
+        n_steps = 4,
+        env = env,
+        use_more_actions = USE_MORE_ACTIONS,
+    )
+    vales.Q.load("prio_agent_self_play_17_2_25")
+    opponents_names.append("Valentin")
+if args.selfplay:
+    opponents.append(copy.deepcopy(agent))
+    opponents_names.append("Self_play")
 
 if opponents == []: 
     logging.error("At least one opponent has to be selected!")
@@ -181,7 +203,15 @@ betas = []
 stats = []
 losses = []
 epsilons = []
-saved_weights = []
+
+if args.weights_for_selfplay != "":
+    if not os.path.isdir(args.weights_for_selfplay):
+        raise ValueError(f"\"{args.weights_for_selfplay}\" is not a valid path for weights_for_selfplay.")
+    exit
+    saved_weights = [f"_initial_{name}" for name in os.listdir(f"{args.weights_for_selfplay}/weights") if os.path.isdir(os.path.join(args.weights_for_selfplay, name))]
+    logging.info(f"Loaded initial weights for selfplay: {saved_weights}")
+else:
+    saved_weights = []
 
 frame_idx = 0
 
@@ -196,16 +226,20 @@ last_save_time = time.time()
 
 for episode in range(max_episodes):
 
-    selected = random.randint(0, len(opponents) - 1)
+    if args.selfplay and saved_weights == []:
+        elected = random.randint(0, len(opponents) - 2)
+    else:
+        selected = random.randint(0, len(opponents) - 1)
     
     opponent = opponents[selected]
     logging.info(opponents_names[selected])
 
-    """if opponents_names[selected] == "Self_play":
-        #self_play_time = time.time()
+    if opponents_names[selected] == "Self_play":
         weights = random.choice(saved_weights)
-        opponent.Q.load(env_name, name = weights)
-       # logging.debug(f" Self play time: {time.time()-self_play_time}")"""
+        if weights.startswith("_initial_"):
+            opponent.Q.load(args.weights_for_selfplay, name = weights)
+        else:
+            opponent.Q.load(env_name, name = weights)
 
     for game in range(games_to_play):
 
@@ -221,22 +255,20 @@ for episode in range(max_episodes):
             done = False
 
             a1 = agent.act(state)
-            if(USE_MORE_ACTIONS):
+            if(agent.use_more_actions):
                 a1_cont = MORE_ACTIONS[a1]
             else: 
                 a1_cont = env.discrete_to_continous_action(a1)
 
-            """if opponents_names[selected] not in ["Random", "Weak", "NonWeak"]:
+            if opponents_names[selected] not in ["Random", "Weak", "NonWeak"]:
                 a2 = opponent.act(obs_agent2, eps = 0, validation = True)
-                if USE_MORE_ACTIONS:
+                if opponent.use_more_actions:
                     a2 = MORE_ACTIONS[a2]
                 else:
                     a2 = env.discrete_to_continous_action(a2)
             else:
-                a2 = opponent.act(obs_agent2)"""
+                a2 = opponent.act(obs_agent2)
             
-            a2 = opponent.act(obs_agent2)
-
             full_action = np.hstack([a1_cont, a2])
 
             start_time = time.time()        # Debbuging
