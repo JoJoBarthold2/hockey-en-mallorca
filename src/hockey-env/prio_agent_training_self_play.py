09d273fb7900f23fc12d8ec20c67e9347779ed12
Schmidtke <valentin.j.schmidt@web.de>
2025-02-23 11:21:19 +0100

    n step 10

--- src/hockey-env/prio_agent_training_self_play.py
+++ src/hockey-env/prio_agent_training_self_play.py
@@ -37,10 +37,10 @@ agent = Prio_DQN_Agent(
         eps=0.01,
         learning_rate=0.0001,
         hidden_sizes=[256, 256],
-        n_steps=1,
-        use_prio = False,
+        n_steps=10,
+        use_prio = True,
     )
   
-env_name = "dqn_agent_self_play_22_02_25_no_prio"
+env_name = "prio_agent_self_play_23_02_25_n_step_10"
 
 train_agent_self_play(agent = agent, use_more_actions = USE_MORE_ACTIONS, seed = SEED_TRAIN_1, env=env, env_name = env_name)
