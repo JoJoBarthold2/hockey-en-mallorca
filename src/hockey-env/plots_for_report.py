d0e17e7e1acdc51b034c2e37fa7f15df9dd7a159
Schmidtke <valentin.j.schmidt@web.de>
2025-02-25 19:18:13 +0100

    a few more plots

--- src/hockey-env/plots_for_report.py
+++ src/hockey-env/plots_for_report.py
@@ -35,6 +35,12 @@ classic_dqn = "../weights/no_prio_agent_23_02_25_n_step_1"
 Combined_n_4 = "../weights/Noisy_Dueling_Double_DQN_Prio_n_step_4_adaptive_training_fixed"
 Combined_n_4_ten_games = "../weights/Noisy_Dueling_Double_DQN_Prio_n_step_4_10_games_only"
 
+
+
+#### Experimentals
+
+bigger_lr = "../weights/Noisy_Dueling_Double_DQN_Prio_n_step_4_lr_0.0005"
+bigger_nn = "../weights/Noisy_Dueling_Double_DQN_Prio_n_step_4_bigger_nn"
 plots_dir = "../../plots"
 def plot_rewards(agents_names, episodes = 5000, name = "rewards"):
     plt.figure(figsize=(10, 6))
@@ -47,12 +53,12 @@ def plot_rewards(agents_names, episodes = 5000, name = "rewards"):
         
      
         plotted_rewards = np_rewards[:int(episodes)]
-        print("================================")
+        """print("================================")
         print(agent_name)
         print(np_rewards.shape)
         print(plotted_rewards.shape)
         
-        print("================================")
+        print("================================")"""
         plt.plot(running_mean(plotted_rewards, 100), label=agent_name)
     plt.xlabel('Episode')
     plt.ylabel('Rewards')
@@ -92,7 +98,10 @@ plot_rewards([(prio_alpha_04, "alpha_0.4_beta_06"),(prio_alpha_02_beta_04, "alph
 plot_rewards([(Combined_n_4, "Combined_n_4"),(tournament_agent_path, "prioritized"), (classic_dqn, "dqn")], episodes = int(2000), name = "rewards_vs_combined")
 plot_rewards([(Combined_n_4,"50 games"),(Combined_n_4_ten_games, "10 games")], episodes = int(2000), name = "rewards_combined_vs_ten_games")
 
+plot_rewards([(bigger_lr, "bigger_lr"),(bigger_nn, "bigger_nn"), (Combined_n_4, "Combined_n_4")], episodes = int(2000), name = "rewards_bigger_lr_nn")
+
 plot_winrate([(tournament_agent_path, "n_step_4"),(n_step_6_path, "n_step_6"),(n_step_1_path, "n_step_1") ], opponent_name=  "Weak", opponent_idx = 1, name = "win_rate_n_steps")
-plot_winrate([(prio_alpha_04, "alpha_0.4_beta_06"),(prio_alpha_02_beta_04, "alpha_0.2_beta_0.4"),(no_prio_n_4, "no_prio_n_4"), (tournament_agent_path, "alpha_02_beta_06"), (classic_dqn, "classic dqn") ], opponent_name=  "Weak", opponent_idx = 1, name = "win_rate_alpha_beta")
-plot_winrate([(Combined_n_4, "Combined_n_4"),(tournament_agent_path, "prioritized"), (classic_dqn, "dqn")], opponent_name=  "Weak", opponent_idx = 1, name = "win_rate_vs_combined")
+plot_winrate([(prio_alpha_04, "alpha_0.4_beta_06"),(prio_alpha_02_beta_04, "alpha_0.2_beta_0.4"),(no_prio_n_4, "no_prio_n_4"), (tournament_agent_path, "alpha_02_beta_06"), (classic_dqn, "classic dqn") ], games = 12000, opponent_name=  "Weak", opponent_idx = 1, name = "win_rate_alpha_beta")
+plot_winrate([(Combined_n_4, "Combined_n_4"),(tournament_agent_path, "prioritized"), (classic_dqn, "dqn")], opponent_name=  "Weak", opponent_idx = 1, name = "win_rate_vs_combined", games = 12000)
 plot_winrate([(Combined_n_4,"50 games"),(Combined_n_4_ten_games, "10 games")], opponent_name=  "Weak", opponent_idx = 1, name = "win_rate_combined_vs_ten_games", games=8000)
+plot_winrate([(bigger_lr, "bigger_lr"),(bigger_nn, "bigger_nn"), (Combined_n_4, "Combined_n_4")], opponent_name=  "Weak", opponent_idx = 1, name = "win_rate_bigger_lr_nn")
