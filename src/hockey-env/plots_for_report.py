03bda06ca7f9772c6c78285c3496793a25f66e25
Schmidtke <valentin.j.schmidt@web.de>
2025-02-26 12:14:06 +0100

    new plots

--- src/hockey-env/plots_for_report.py
+++ src/hockey-env/plots_for_report.py
@@ -99,7 +99,7 @@ plot_rewards([(tournament_agent_path, "4 Steps"),(n_step_6_path, "6 Steps"),(n_s
 plot_rewards([(tournament_agent_path, "Alpha:0.2 Beta:0.6"),(prio_alpha_04, "Alpha:0.4 Beta:0.6"), (classic_dqn, "DQN"),(prio_alpha_02_beta_04, "Alpha:0.2 Beta:0.4") ], episodes = int(1500), name = "rewards_alpha_beta")
 plot_rewards([(tournament_agent_path, "4 N steps with PER "),(no_prio_n_4, "4 N steps without PER"), (classic_dqn, "DQN")], episodes = int(2000), name = "rewards_no_prio")
 
-plot_rewards([(tournament_agent_path, "PER + 4-Step"),(Combined_n_4, "Combined"), (classic_dqn, "DQN")], episodes = int(2000), name = "rewards_vs_weak")
+plot_rewards([(tournament_agent_path, "PER + 4-Step"),(Combined_n_4, "Combined"), (classic_dqn, "DQN"), (bigger_nn, "Combined rectified")], episodes = int(2000), name = "rewards_vs_weak")
 plot_rewards([(Combined_n_4,"50 games"),(Combined_n_4_ten_games, "10 games")], episodes = int(2000), name = "rewards_combined_vs_ten_games")
 
 plot_rewards([(bigger_lr, "bigger_lr"),(bigger_nn, "bigger_nn"), (Combined_n_4, "Combined_n_4")], episodes = int(2000), name = "rewards_bigger_lr_nn")
